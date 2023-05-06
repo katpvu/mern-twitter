7a12cfb536c782be91351cbab1a7ffa300648ebc
katpvu <vu.kathy98@gmail.com>
2023-05-06 16:03:46 -0700

    add csrf

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET tweets  */
 router.get('/', function(req, res, next) {
   res.json({
-    message: "GET /api/tweets"
+    message: "GET /api/csrf"
   });
 });
 
