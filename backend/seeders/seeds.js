b95fe4fec0ffc33bf7924b0172f91db9e00f17be
katpvu <vu.kathy98@gmail.com>
2023-05-06 19:05:40 -0700

    add seeds

---
+++ backend/seeders/seeds.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { mongoURI: db } = require('../config/keys.js');
+const User = require('../models/User');
+const Tweet = require('../models/Tweet');
+const bcrypt = require('bcryptjs');
+const { faker } = require('@faker-js/faker');
+
+// create users
+const users = [];
+
+users.push(
+    new User ({
+        username: 'demo-user',
+        email: 'demo-user@appacademy.io',
+        hashedPassword: bcrypt.hashSync('starwars', 10)
+    })
+)
+
+for (let i = 1; i < NUM_SEED_USERS; i++) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    users.push (
+        new User ({
+            username: faker.internet.userName(firstName, lastName),
+            email: faker.internet.email(firstName, lastName),
+            hashedPassword: bcrypt.hashSync(faker.internet.password(), 10)
+        })
+    )
+}
+
+// create tweets
+const tweets = [];
+for (let i = 0; i < NUM_SEED_TWEETS; i++) {
+    tweets.push(
+        new Tweet({
+            text: faker.hacker.phrase(),
+            author: users[Math.floor(Math.random() * NUM_SEED_USERS)]._id
+        })
+    )
+}
